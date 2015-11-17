f31e9226c1795b16ba8c2388fe60fd64229c733d
Benjamin Aplin <benjaminaplin@gmail.com>
2015-11-17 00:23:35 -0500

    make modal

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 console.log('main is linked');
 
-$('div.event-image').on('click', function(e){
-  console.log('penis')
+$('div.event-image img').on('click', function(e){
+  console.log('e.target', e.target);
+  slideShow.toggle();
+  var targetImage = $(this).attr('src')
+  console.log('targetImage', targetImage);
+  $('.slide-show-body').html('');
+  $('.slide-show-body').append('<div><img src="' + targetImage + '"></div>')
 })
+
+var slideShow = $('#slide-show');
+var closeButton = $('#close-slide-show');
+
+closeButton.on('click', function() {
+  slideShow.toggle();
+})
+
+
+//SLIDESHOW
+//once in modal, on click of arrow, animage one slide out one slide in
+//use masking? so that images hidden as they flow out? 
+//array of images? on click, inject next item in array into html?s
+
+
