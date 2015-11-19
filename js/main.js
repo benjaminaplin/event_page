6163126887bb6da7d95036e17a130f1bbc1e4277
Benjamin Aplin <benjaminaplin@gmail.com>
2015-11-18 23:01:23 -0500

    add large img functionality to slide show

--- js/main.js
+++ js/main.js
@@ -1,42 +1,53 @@
+//Project Name: Event Page
+//Client: PureWow
+//Author: Benjamin Aplin
+//
 console.log('main.js is linked');
-
-$('div.event-image img').on('click', function(e){
-  slideShow.toggle();
-  var targetImage = $(this).attr('src')
-  $('.slide-show-body').html('');
-  $('.slide-show-body').append('<div  style="border: 0px solid; outline: 0;"><img  class="current-image" src="' + targetImage + '"></div>')
-})
-
-var slideShow = $('#slide-show');
-var closeButton = $('#close-slide-show');
-
-closeButton.on('click', function() {
-  slideShow.toggle();
-})
-
-var changeSlide = function(num, currentImage){
-  var images = ['./images/1.jpg', './images/2.jpg', './images/3.jpg', './images/4.jpg', './images/5.jpg', './images/6.jpg', './images/7.jpg', './images/8.jpg', './images/9.jpg', './images/10.jpg', './images/11.jpg', './images/12.jpg', './images/13.jpg', './images/14.jpg', './images/3.jpg']
-  var currentIndex = images.indexOf(currentImage)
-  var nextIndex = currentIndex + num;
-  if(nextIndex > images.length-1){
-    nextIndex = 0;
-  }
-  if(nextIndex < 0){
-    nextIndex = images.length-1;
-  }
-  $('.slide-show-body').html('');
-  $('.slide-show-body').append('<div><img class="current-image" src="' + images[nextIndex] + '"></div>')
-}
 
 var currentImage;
 
-$('#back-button').on('click', function(e){
+$('div.event-image img').on('click', function(e){
+  $('#slide-show').toggle();
+  var targetImage = $(this).attr('src');
+  _.each(slideShowContent, function(e, i, l) {
+    if(e.smImg === targetImage){
+      $('.slide-show-body').html('');
+      $('.slide-show-body').append('<div  style="border: 0px solid; outline: 0;"><img  class="current-image" src="' + e.lgImg + '"></div>');
+      $('#slide-show-text').html('').append('<div>' + e.slideShowText + '</div>')   
+    }
+  })
+})
+
+$('#close-slide-show').on('click', function() {
+  $('#slide-show').toggle();
+})
+
+var changeSlide = function(num, currentImage){
+  _.each(slideShowContent, function(e, i, l) {
+    if(e.lgImg === currentImage){
+      var nextIndex = i + num;
+      if(nextIndex > slideShowContent.length-1){
+        nextIndex = 0;
+      }
+      if(nextIndex < 0){
+        nextIndex = slideShowContent.length-1;
+      }
+      $('.slide-show-body').html('').append('<div><img class="current-image" src="' + slideShowContent[nextIndex].lgImg + '"></div>')
+      $('#slide-show-text').html('').append('<div>' + slideShowContent[nextIndex].slideShowText + '</div>')
+    }
+  })
+}
+
+$('#back-button').on('click', function(){
   currentImage = $('.current-image').attr('src');
   changeSlide(-1, currentImage);
 })
 
-$('#forward-button').on('click', function(e){
+$('#forward-button').on('click', function(){
   currentImage = $('.current-image').attr('src');
   changeSlide(1, currentImage);
 })
 
+
+  // var currentSlideShowImage = _.findWhere(slideShowContent, {smImg: currentImage})
+  // console.log('currentSlideShowImage.lgImg', currentSlideShowImage.lgImg)
